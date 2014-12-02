c5d34dd8c3db5b34b68f65bb23b14daef1428ecc
Salvador Diaz <diaz.salvador@gmail.com>
2014-12-02 23:13:07 +0100

    Annotation processors work but tests generated in wrong folder

--- mdl4ui-fields/src/test/java/org/mdl4ui/ui/sample/FieldMessageSampleTest.java
+++ mdl4ui-fields/src/test/java/org/mdl4ui/ui/sample/FieldMessageSampleTest.java
@@ -1,8 +1,8 @@
 package org.mdl4ui.ui.sample;
 
+import static org.mdl4ui.ui.sample.EGroupSample.*;
 import static org.mdl4ui.ui.sample.EFieldSample.*;
 import static org.mdl4ui.ui.sample.EBlockSample.*;
-import static org.mdl4ui.ui.sample.EGroupSample.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -33,7 +33,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(PERSONAL_INFORMATIONS);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(PERSONAL_INFORMATIONS);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(PERSONAL_INFORMATIONS);
-        assertEquals("My Informations", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -44,7 +44,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(MAIL_SETTINGS);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(MAIL_SETTINGS);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(MAIL_SETTINGS);
-        assertEquals("My Settings and Email", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -55,7 +55,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(ACCOUNT);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(ACCOUNT);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(ACCOUNT);
-        assertEquals("My Account", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -66,7 +66,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(PHONE_SETTINGS);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(PHONE_SETTINGS);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(PHONE_SETTINGS);
-        assertEquals("My Settings and Phone Number", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -78,9 +78,9 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(FIRST_NAME);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(FIRST_NAME);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(FIRST_NAME);
-        assertEquals("First Name", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
-        assertEquals("John", messagePlaceHolder);
+        assertNull(messagePlaceHolder);
 
     }
 
@@ -89,9 +89,9 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(LAST_NAME);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(LAST_NAME);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(LAST_NAME);
-        assertEquals("Last Name", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
-        assertEquals("Doe", messagePlaceHolder);
+        assertNull(messagePlaceHolder);
 
     }
 
@@ -100,9 +100,9 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(EMAIL);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(EMAIL);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(EMAIL);
-        assertEquals("Email", messageLabel);
-        assertEquals("You will receive a email to confirm your inscription", messageHelp);
-        assertEquals("john@doe.com", messagePlaceHolder);
+        assertNull(messageLabel);
+        assertNull(messageHelp);
+        assertNull(messagePlaceHolder);
 
     }
 
@@ -111,9 +111,9 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(BIRTHDATE);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(BIRTHDATE);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(BIRTHDATE);
-        assertEquals("Birth date", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
-        assertEquals("dd/mm/yyyy", messagePlaceHolder);
+        assertNull(messagePlaceHolder);
 
     }
 
@@ -122,7 +122,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(LANGUAGE);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(LANGUAGE);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(LANGUAGE);
-        assertEquals("Language", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -133,7 +133,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(EMAIL_ACCEPTED);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(EMAIL_ACCEPTED);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(EMAIL_ACCEPTED);
-        assertEquals("I accept to receive email", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -144,7 +144,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(EMAILS_PREFERENCES);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(EMAILS_PREFERENCES);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(EMAILS_PREFERENCES);
-        assertEquals("Email preference", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -155,7 +155,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(MAX_WEEKLY_EMAILS);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(MAX_WEEKLY_EMAILS);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(MAX_WEEKLY_EMAILS);
-        assertEquals("Emails limit / week", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -166,7 +166,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(TIMEZONE);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(TIMEZONE);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(TIMEZONE);
-        assertEquals("Timezone", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -177,7 +177,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(LOGIN);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(LOGIN);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(LOGIN);
-        assertEquals("Login", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -188,7 +188,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(PASSWORD);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(PASSWORD);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(PASSWORD);
-        assertEquals("Password", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -199,7 +199,7 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(PASSWORD_CONFIRMATION);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(PASSWORD_CONFIRMATION);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(PASSWORD_CONFIRMATION);
-        assertEquals("Confirm your password", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
         assertNull(messagePlaceHolder);
 
@@ -210,9 +210,9 @@ public final class FieldMessageSampleTest {
         final String messageLabel = BundleFieldFactory.INSTANCE.getLabel(PHONE_NUMBER);
         final String messageHelp = BundleFieldFactory.INSTANCE.getHelp(PHONE_NUMBER);
         final String messagePlaceHolder = BundleFieldFactory.INSTANCE.getPlaceHolder(PHONE_NUMBER);
-        assertEquals("Phone Number", messageLabel);
+        assertNull(messageLabel);
         assertNull(messageHelp);
-        assertEquals("01 02 03 04 06", messagePlaceHolder);
+        assertNull(messagePlaceHolder);
 
     }
 
